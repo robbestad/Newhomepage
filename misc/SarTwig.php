a8022be5af7fc00b9a5b5533cf6f871a78c9af1f
Sven Anders Robbestad <robbestad@gmail.com>
2013-12-20 07:26:33 +0100

    added SarTwig to misc

---
+++ misc/SarTwig.php
@@ -0,0 +1,22 @@
+<?php
+namespace ZfcTwig\Twig\Extension;
+
+use Twig_Extension;
+use ZfcTwig\View\Renderer\TwigRenderer;
+
+class SarTwig extends Twig_Extension
+{
+
+    public function getName()
+    {
+         return 'sar-twig';
+    }
+
+
+    public function getGlobals() {
+        return array(
+            'session'   => $_SESSION,
+            'slug' => trim(($_SERVER['REQUEST_URI'] ), '/')
+        ) ;
+    }
+}
